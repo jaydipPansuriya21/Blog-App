2f931dccff57f67128e3bce69af983f3630f83ff
ImJaydip <jaydip.pansuriya@oneadvanced.com>
2021-02-21 16:11:11 +0530

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include SessionsHelper
 
-  def hello
-    render html: "Hello I am hello page ..."
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :users
 
   root 'static_pages#home'
+
+
+  match '/signin',  to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/signup', to: 'users#new', via: [:get]
 
